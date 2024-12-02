e7e5455b48838f5912dc885495d08a3584b77672
D3ds3c007 <nasforger42@gmail.com>
2024-12-02 14:25:35 +0300

    reset leaderboard

--- sql.sql
+++ sql.sql
@@ -188,3 +188,148 @@ VALUES
     (187, 'Yemeni'),
     (188, 'Zambian'),
     (189, 'Zimbabwean');
+
+
+INSERT INTO public."Attendances" (
+    "StaffId", "Date", "ClockInTime", "ClockOutTime", "LastDetectedTime", "Remark"
+)
+VALUES
+    -- Records for December 1
+    (60, '2024-12-01 22:00:00+01', '07:05:00', '09:45:00', '2024-12-01 08:30:00+01', NULL),
+    (63, '2024-12-01 22:00:00+01', '07:15:00', '09:50:00', '2024-12-01 08:35:00+01', 'Traffic delay'),
+    (66, '2024-12-01 22:00:00+01', '07:25:00', '09:55:00', '2024-12-01 08:40:00+01', NULL),
+
+    -- Records for December 2
+    (60, '2024-12-02 22:00:00+01', '07:10:00', '09:40:00', '2024-12-02 08:20:00+01', 'Late due to weather'),
+    (63, '2024-12-02 22:00:00+01', '07:20:00', '09:50:00', '2024-12-02 08:25:00+01', NULL),
+    (66, '2024-12-02 22:00:00+01', '07:30:00', '09:45:00', '2024-12-02 08:30:00+01', 'Missed bus'),
+
+    -- Records for December 3
+    (60, '2024-12-03 22:00:00+01', '07:08:00', '09:42:00', '2024-12-03 08:25:00+01', NULL),
+    (63, '2024-12-03 22:00:00+01', '07:18:00', '09:52:00', '2024-12-03 08:35:00+01', 'Heavy rain'),
+    (66, '2024-12-03 22:00:00+01', '07:28:00', '09:48:00', '2024-12-03 08:40:00+01', NULL),
+
+    -- Records for December 4
+    (60, '2024-12-04 22:00:00+01', '07:12:00', '09:38:00', '2024-12-04 08:22:00+01', NULL),
+    (63, '2024-12-04 22:00:00+01', '07:22:00', '09:48:00', '2024-12-04 08:28:00+01', 'Late due to traffic'),
+    (66, '2024-12-04 22:00:00+01', '07:32:00', '09:42:00', '2024-12-04 08:33:00+01', NULL)
+
+
+
+
+
+    WITH months AS (
+                              SELECT generate_series(
+                                  date_trunc('month', (SELECT MIN("Date") FROM public."Attendances")),
+                                  date_trunc('month', CURRENT_DATE),
+                                  '1 month'
+                              ) AS month
+                          ),
+                          lateness AS (
+                              SELECT
+                                  st."IdStaff" AS "StaffId",
+                                  EXTRACT(YEAR FROM m.month) AS Year,
+                                  EXTRACT(MONTH FROM m.month) AS Month,
+                                  st."StaffName",
+                                  COUNT(CASE
+                                      WHEN a."ClockInTime" IS NOT NULL
+                                      AND a."ClockInTime" > s."Begin"
+                                      THEN 1
+                                      ELSE NULL
+                                  END) AS latenesscount
+                              FROM
+                                  public."Staffs" st
+                              CROSS JOIN months m
+                              LEFT JOIN public."Attendances" a ON st."IdStaff" = a."StaffId"
+                                      AND DATE_TRUNC('month', a."Date") = DATE_TRUNC('month', m.month)
+                              LEFT JOIN public."Schedules" s ON st."IdStaff" = s."StaffId"
+                                      AND EXTRACT(DOW FROM a."Date") = s."DayOfWeek"
+                              WHERE
+                                  m.month = CURRENT_DATE
+                              GROUP BY st."IdStaff", Year, Month, st."StaffName"
+                          ),
+                          absence AS (
+                              SELECT
+                                  wd."IdStaff",
+                                  wd."StaffName",
+                                  COUNT(CASE WHEN a."ClockInTime" IS NULL AND t."IdTimeOff" IS NULL THEN 1 END) AS AbsenceCount
+                              FROM (
+                                  SELECT
+                                      st."IdStaff",
+                                      st."StaffName",
+                                      d.day
+                                  FROM public."Staffs" st
+                                  CROSS JOIN (
+                                      SELECT generate_series(
+                                          date_trunc('month', CURRENT_DATE),
+                                          date_trunc('month', CURRENT_DATE) + interval '1 month' - interval '1 day',
+                                          '1 day'
+                                      ) AS day
+                                  ) d
+                                  JOIN public."Schedules" s
+                                      ON st."IdStaff" = s."StaffId"
+                                      AND EXTRACT(DOW FROM d.day) = s."DayOfWeek"
+                              ) wd
+                              LEFT JOIN public."Attendances" a
+                                  ON wd."IdStaff" = a."StaffId"
+                                  AND DATE(a."Date") = wd.day
+                              LEFT JOIN public."TimeOffs" t
+                                  ON wd."IdStaff" = t."StaffId"
+                                  AND wd.day BETWEEN t."BeginTimeOff" AND t."EndTimeOff"
+
+                              GROUP BY wd."IdStaff", wd."StaffName"
+                          ),
+                          ontime AS (
+                              SELECT
+                                  st."IdStaff" AS "StaffId",
+                                  EXTRACT(YEAR FROM m.month) AS Year,
+                                  EXTRACT(MONTH FROM m.month) AS Month,
+                                  st."StaffName",
+                                  COUNT(CASE
+                                      WHEN a."ClockInTime" IS NOT NULL
+                                      AND a."ClockInTime" <= s."Begin"
+                                      THEN 1
+                                      ELSE NULL
+                                  END) AS OnTimeCount
+                              FROM
+                                  public."Staffs" st
+                              CROSS JOIN months m
+                              LEFT JOIN public."Attendances" a ON st."IdStaff" = a."StaffId"
+                                      AND DATE_TRUNC('month', a."Date") = DATE_TRUNC('month', m.month)
+                              LEFT JOIN public."Schedules" s ON st."IdStaff" = s."StaffId"
+                                      AND EXTRACT(DOW FROM a."Date") = s."DayOfWeek"
+                              WHERE
+                                  m.month = CURRENT_DATE
+                              GROUP BY st."IdStaff", Year, Month, st."StaffName"
+                          )
+                          SELECT
+                              ROW_NUMBER() OVER (ORDER BY
+                                  CASE
+                                      WHEN COALESCE(l."latenesscount", 0) = 0 AND COALESCE(a."absencecount", 0) = 0 THEN 1
+                                      WHEN COALESCE(l."latenesscount", 0) <= 2 AND COALESCE(a."absencecount", 0) <= 1 THEN 2
+                                      WHEN COALESCE(l."latenesscount", 0) <= 4 AND COALESCE(a."absencecount", 0) <= 2 THEN 3
+                                      WHEN COALESCE(l."latenesscount", 0) <= 6 AND COALESCE(a."absencecount", 0) <= 3 THEN 4
+                                      ELSE 5
+                                  END,
+                                  l."latenesscount", a."absencecount"
+                              ) AS rank,
+                              s."IdStaff" AS "StaffId",
+                              s."StaffName",
+                              s."Matricule",
+                              COALESCE(l.Year, EXTRACT(YEAR FROM CURRENT_DATE)) AS Year,
+                              COALESCE(l.Month, EXTRACT(MONTH FROM CURRENT_DATE)) AS Month,
+                              COALESCE(l."latenesscount", 0) AS latenesscount,
+                              COALESCE(a."absencecount", 0) AS absencecount,
+                              COALESCE(ot."ontimecount", 0) AS ontimecount,
+                              CASE
+                                  WHEN COALESCE(l."latenesscount", 0) = 0 AND COALESCE(a."absencecount", 0) = 0 THEN 'Excellent'
+                                  WHEN COALESCE(l."latenesscount", 0) <= 2 AND COALESCE(a."absencecount", 0) <= 1 THEN 'Good'
+                                  WHEN COALESCE(l."latenesscount", 0) <= 4 AND COALESCE(a."absencecount", 0) <= 2 THEN 'Average'
+                                  WHEN COALESCE(l."latenesscount", 0) <= 6 AND COALESCE(a."absencecount", 0) <= 3 THEN 'Fair'
+                                  ELSE 'Poor'
+                              END AS PunctualityRating
+                          FROM public."Staffs" s
+                          LEFT JOIN lateness l ON s."IdStaff" = l."StaffId"
+                          LEFT JOIN absence a ON s."IdStaff" = a."IdStaff"
+                          LEFT JOIN ontime ot ON s."IdStaff" = ot."StaffId"
+                          ORDER BY rank;
